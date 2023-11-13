93738a5b0dfd5d743ebf824d81aa4b7cf1484346
Fidji32 <matthis.vandevyver@gmail.com>
2023-11-13 16:28:43 +0100

    init

---
+++ projet/backend/test.js
@@ -0,0 +1,8 @@
+// test.js
+const assert = require('assert');
+
+describe('Test d\'addition', function () {
+  it('2 + 2 devrait être égal à 4', function () {
+    assert.strictEqual(2 + 2, 4);
+  });
+});
